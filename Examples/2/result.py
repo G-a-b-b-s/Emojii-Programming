2a05eecce0f7ea1d048049b5eaceb7f60ca5d862
martynabaran <baran.martyna04@gmail.com>
2024-05-18 12:13:46 +0200

    some updates in gramma: cerated a gramma with unicode name added part of listener

---
+++ Examples/2/result.py
@@ -0,0 +1,7 @@
+if a>b:
+	print("a wieksze od b")
+elif a<b:
+	print("a mniejsze od b")
+else:
+	print("a i b sa rowne")
+
---
+++ result.py
@@ -0,0 +1,8 @@
+if a>b:
+	print("a wieksze od b")
+elif a<b:
+	print("a mniejsze od b")
+
+else:
+	print("a i b sa rowne")
+
